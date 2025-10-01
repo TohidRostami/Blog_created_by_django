a6573c3ffb9e3e007e6a60f50c1472785d8f674c
TohidRostamii <tohid@greenhabitat.at>
2025-10-01 14:10:33 +0330

    added comment form to post page

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
+from .forms import CommentForm
 from .models import Post
+
 all_posts=[]
 
-def get_date(post):
-    return post["date"]
-
 # Create your views here.
-def starting_page(request):
-    latest_posts =Post.objects.all().order_by("-date")[:3]
-    return render(request,"blog/index.html",{
-        "posts": latest_posts
-    })
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request,"blog/all-posts.html",{
-        "posts": all_posts
-    })
+class StratingPageView(ListView):
+    template_name="blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html",{
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+class AllPostsView(ListView):
+    template_name="blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+    
+class SingelPostView(DetailView):
+    template_name="blog/post-detail.html"
+    model=Post
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
+        return context
